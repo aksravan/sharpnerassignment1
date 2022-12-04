c422687a9408831f4681f8874941b99d2c0fcb9e
akshaykumar <akshaykumar8734@gmail.com>
2022-12-04 17:43:29 +0530

    creating nodes

--- dom.js
+++ dom.js
@@ -53,3 +53,54 @@ for(let i in listItem){
 }
 listItem[1].style.color = "green";
 listItem[2].style.visibility = "hidden";
+
+
+var itemList = document.querySelector("#items");
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode.parentNode);
+
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
+
+console.log(itemList.childNodes);
+
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
+
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello 1';
+
+console.log(itemList.lastChild);
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 4';
+
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+
+console.log(itemList.previousSibling);
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = "green";
+
+var newDiv = document.createElement("div");
+
+newDiv.className = "hello";
+
+newDiv.id = "hello1";
+newDiv.setAttribute("title", "Hello Div");
+
+var newDivText = document.createTextNode("Hello World");
+
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+
+console.log(newDiv);
+
+newDiv.style.fontSize = "30px";
+
+container.insertBefore(newDiv, h1);
